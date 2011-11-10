25269100d6d3c39a06e9ce49fd95538db7ebf3d6
Jason Galea <jason@lecstor.com>
2011-11-10 22:15:33 +1000

    extend View::TT to process output with PDF::Boxer

--- lib/Catalyst/View/PDFBoxer.pm
+++ lib/Catalyst/View/PDFBoxer.pm
@@ -1,5 +1,24 @@
-use strict;
-use warnings;
 package Catalyst::View::PDFBoxer;
+use Moose;
+
+extends 'Catalyst::View::TT';
+
+use PDF::Boxer;
+use PDF::Boxer::SpecParser;
+
+before process => sub {
+    my ($self, $c) = @_;
+    unless ( $c->response->content_type ) {
+        $c->response->content_type('application/pdf; charset=utf-8');
+    }
+};
+
+after process => sub {
+    my ($self, $c) = @_;
+    my $spec = PDF::Boxer::SpecParser->new->parse($c->response->body);
+    my $boxer = PDF::Boxer->new;
+    $boxer->add_to_pdf($spec);
+    $c->response->body($boxer->doc->pdf->stringify);
+};
 
 1;
---
+++ t/lib/TestApp/View/PDFBoxer.pm
@@ -0,0 +1,6 @@
+package TestApp::View::PDFBoxer;
+
+use strict;
+use base 'Catalyst::View::PDFBoxer';
+
+1;
